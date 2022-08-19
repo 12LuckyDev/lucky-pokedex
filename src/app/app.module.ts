c6b5e8d29d613a68af2632ffaeefa85d0a6e54d2
12LuckyDev <12luckydev@gmail.com>
2022-08-19 14:38:30 +0200

    General refactor #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { PokedexTableComponent } from './pokedex-table/pokedex-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { VariantsTableComponent } from './pokedex-table/variants-table/variants-table.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PokedexOptionsComponent } from './pokedex-table/pokedex-options/pokedex-options.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+
+import { AppComponent } from './app.component';
+import { PokedexTableComponent } from './pokedex-table/pokedex-table.component';
+import { VariantsTableComponent } from './pokedex-table/variants-table/variants-table.component';
+import { PokedexOptionsComponent } from './pokedex-table/pokedex-options/pokedex-options.component';
 import { PokeImgComponent } from './pokedex-table/poke-selection-check/poke-img/poke-img.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { PokedexBaseComponent } from './pokedex-table/pokedex-base-component/pokedex-base.component';
 import { PokeSelectionCheckComponent } from './pokedex-table/poke-selection-check/poke-selection-check.component';
 import { PokedexSearchComponent } from './pokedex-table/pokedex-search/pokedex-search.component';
@@ -27,17 +34,29 @@ import { PokeSelectionAllComponent } from './pokedex-table/poke-selection-all/po
 import { PokedexTypesComponent } from './pokedex-table/pokedex-types/pokedex-types.component';
 import { PokedexTypeComponent } from './pokedex-table/pokedex-types/pokedex-type/pokedex-type.component';
 import { PokeStatisticsComponent } from './pokedex-table/poke-statistics/poke-statistics.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { PokeStatisticsBarsComponent } from './pokedex-table/poke-statistics/poke-statistics-bars/poke-statistics-bars.component';
 import { PokeDetailedStatisticsComponent } from './pokedex-table/poke-statistics/poke-detailed-statistics/poke-detailed-statistics.component';
-import { MatDialogModule } from '@angular/material/dialog';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
 }
+
+const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatCheckboxModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatCardModule,
+  MatDividerModule,
+  MatIconModule,
+  MatProgressBarModule,
+  MatDialogModule,
+];
 
 @NgModule({
   declarations: [
@@ -58,21 +77,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatCheckboxModule,
-    MatExpansionModule,
-    FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatCardModule,
-    MatDividerModule,
-    MatIconModule,
-    MatProgressBarModule,
     FlexLayoutModule,
     HttpClientModule,
     TranslateModule.forRoot({
@@ -82,7 +87,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
         deps: [HttpClient],
       },
     }),
-    MatDialogModule,
+    ...MATERIAL_MODULES,
   ],
   providers: [PokedexBaseComponent],
   bootstrap: [AppComponent],
